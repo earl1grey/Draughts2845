240d4eed7c87829cf36484b291e6d9c4b748363c
earl1grey <153310728+earl1grey@users.noreply.github.com>
2024-05-19 00:33:11 +0300

    Added graphics, music, restart option

--- main.py
+++ main.py
@@ -17,9 +17,17 @@ pygame.display.set_caption('Шашки') #Название игры.
 font = pygame.font.SysFont(None, 100)
 font_small = pygame.font.SysFont(None, 50)
 
+bg = pygame.image.load('assets/background_menu.png')
+black = pygame.image.load('assets/black.png')
+white = pygame.image.load('assets/white.png')
+
+pygame.mixer.init()
+music = pygame.mixer.music.load('assets/music.ogg')
+pygame.mixer.music.play(-1)
+
 turn = LIGHT_BLACK
 
-def draw_text(text, font, color, surface, x, y):
+def draw_text(text, font, color, surface, x, y): #Отрисовка текста
     textobj = font.render(text, 1, color)
     textrect = textobj.get_rect()
     textrect.topleft = (x, y)
@@ -27,18 +35,22 @@ def draw_text(text, font, color, surface, x, y):
 
 click = False
 
-def main_menu():
+def main_menu(): #Главное меню
     global click
     while True:
 
-        WINDOW.fill((255, 255, 255))
+        WINDOW.blit(bg, (0, 0))
         draw_text('Шашки', font, (0, 0, 0), WINDOW, 282, 100)
 
         mx, my = pygame.mouse.get_pos()
 
-        button_1 = pygame.Rect(260, 200, 300, 100)
-        button_2 = pygame.Rect(260, 350, 300, 100)
-        button_3 = pygame.Rect(260, 500, 300, 100)
+        play = pygame.image.load ('assets/play.png')
+        option = pygame.image.load ('assets/option.png')
+        exit = pygame.image.load ('assets/exit.png')
+
+        button_1 = WINDOW.blit(play, (260, 200))
+        button_2 = WINDOW.blit(option, (260, 350))
+        button_3 = WINDOW.blit(exit, (260, 500))
 
         if button_1.collidepoint((mx, my)):
             if click:
@@ -52,10 +64,6 @@ def main_menu():
         if button_3.collidepoint((mx, my)):
             if click:
                 pygame.quit()
-
-        pygame.draw.rect(WINDOW, (255, 0, 0), button_1)
-        pygame.draw.rect(WINDOW, (255, 0, 0), button_2)
-        pygame.draw.rect(WINDOW, (255, 0, 0), button_3)
 
         click = False
         for event in pygame.event.get():
@@ -86,10 +94,11 @@ def main(): #Запуск игры. Функция цикла событий. О
     
     while run:
         speed.tick(FPS) #Отвечает за количество кадров в секунду.
-        
+
         if game.pobeda() != None: #Отвечает за окончание игры (победу в игре).
-            print(game.pobeda())
-            run = False
+            restart_panel()
+            mainClock.tick(1)
+            restart()
 
         for event in pygame.event.get(): #Набор событий.
             if event.type == pygame.QUIT: #Отвечает за выход из программы.
@@ -105,24 +114,25 @@ def main(): #Запуск игры. Функция цикла событий. О
                 game.select(row, col)
 
         game.updating_game()
-    
+
     pygame.quit()
 
-def options():
+
+def options(): #Меню настройки
     global click
     running = True
     while running:
+        WINDOW.blit(bg, (0, 0))
+        
         draw_text('Настройки', font, (0, 0, 0), WINDOW, 230, 100)
         
         draw_text('Кто ходит первым:', font_small, (0, 0, 0), WINDOW, 250, 200)
         
         mx, my = pygame.mouse.get_pos()
 
-        button_4 = pygame.Rect(260, 250, 100, 100)
-        pygame.draw.rect(WINDOW, (255, 0, 0), button_4)
+        button_4 = WINDOW.blit(white, (260, 250))
         
-        button_5 = pygame.Rect(460, 250, 100, 100)
-        pygame.draw.rect(WINDOW, (255, 0, 0), button_5)
+        button_5 = WINDOW.blit(black, (460, 250))
 
         if button_4.collidepoint((mx, my)):
             global turn
@@ -134,19 +144,47 @@ def options():
                 turn = LIGHT_BLACK
 
         click = False
+
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
+
             if event.type == KEYDOWN:
                 if event.key == K_ESCAPE:
                     running = False
+
             if event.type == MOUSEBUTTONDOWN:
                 if event.button == 1:
                     click = True
 
         pygame.display.update()
         mainClock.tick(60)
+
+def restart_panel(): #Экран рестарта
+    global mainClock
+
+    WINDOW.fill((20, 20, 20))
+
+    font = pygame.font.SysFont('Arial', 32)
+    text = font.render('Нажмите пробел для рестарта', True, (255, 255, 255))
+    textRect = text.get_rect()
+    textRect.center = (WIDTH//2, HEIGHT//2)
+
+    WINDOW.blit(text, textRect)
+
+    pygame.display.update()
+
+def restart(): #Функция рестарта
+    for event in pygame.event.get():
+            if event.type == KEYDOWN:
+                if event.key == K_SPACE:
+                    main()
+                if event.type == K_ESCAPE:
+                    quit()
+
+            if event.type == pygame.QUIT:
+                quit()
 
 main_menu()
 
